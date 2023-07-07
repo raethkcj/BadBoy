4bfd23b20ed4188b039d80ec7b70c2b3fe6fba61
Casey Raethke <casey.j.raethke@gmail.com>
2023-07-07 14:09:33 -0500

    Modify for debugging

--- Generate.lua
+++ Generate.lua
@@ -1,6 +1,8 @@
 
 local _, t = ...
 
+BADBOY_FILTERS = t
+
 t.gnt = function(u, ...)
 	local select, strsplit, tonumber, n, char = select, string.split, tonumber, C_Map.GetMapInfo(u).parentMapID, string.char
 	for i = 1, select("#", ...) do
@@ -30,15 +32,32 @@ t.gnt = function(u, ...)
 	t.gnt = nil
 end
 
+local scores = {
+	10,
+	10,
+	1,
+	2,
+	-2,
+	3
+}
+
 local strfind = string.find
 t.is = function(msg)
 	local a = 0
+	local matches = {}
 	for i = 1, 6 do
 		for j=1, #t[i] do
 			if strfind(msg, t[i][j]) then
+				matches[t[i][j]] = scores[i]
 				a = i>5 and a+3 or i>4 and a-1 or i>3 and a+2 or i>2 and a+1 or a+10
 			end
 		end
+	end
+	if BADBOY_DEBUG and a > 0 and a < 3 then
+		matches["TOTAL"] = a
+		DevTools_Dump({
+			[msg] = matches
+		})
 	end
 	return a > 3
 end
